42338e66b9220bfe69ebba0f664837b50d76f8e1
llozpk <igor7713hotnews@gmail.com>
2020-11-25 12:26:19 -0300

    Inicio do Fim

---
+++ Assets/Scripts/LoadNewArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNewArea : MonoBehaviour{
+
+  public string LevelToLoad;
+    // Start is called before the first frame update
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+
+      if(other.gameObject.name == "Player"){
+        Application.LoadLevel(LevelToLoad);
+      }
+
+    }
+
+}
